1a087779c5249c514bb29931a8ea98de5edde328
Mirogniew <mirogniewny@gmail.com>
2022-06-14 19:18:01 +0200

    Issue #3 - Implementation of WWO API response structure

--- src/main/java/org/example/WWO/Weather.java
+++ src/main/java/org/example/WWO/Weather.java
@@ -2,7 +2,13 @@ package org.example.WWO;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import java.util.List;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Weather {
     public String date;
+    public String avgtempC;
+    public String maxtempC;
+    public String mintempC;
+    public List<Astronomy> astronomy;
 }
